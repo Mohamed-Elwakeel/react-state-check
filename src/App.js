bfd58dfe482b9e4ddf7122c1100d676b25e2dd9e
Mohamed Elwakeel <mo7mdhassan7@gmail.com>
2023-05-13 19:39:00 +0300

    added the js code and the functions needed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
+import ME from "../src/assets/me.jpg";
 
 function App() {
-  return <p>Hello World!!</p>;
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  return (
+    <div>
+      {isEnabled && (
+        <div>
+          <h1>FullName: Mohamed Hassan Elwakeel</h1>
+          <h2>Profession: Civil Engineer</h2>
+          <p>
+            Bio: A civil engineer graduate who is aspiring to be a fullstack web
+            developer.
+          </p>
+          <img src={ME} alt="Photo of me!!" />
+        </div>
+      )}
+
+      <button onClick={() => setIsEnabled(!isEnabled)}>Toggle State</button>
+    </div>
+  );
 }
 
 export default App;
